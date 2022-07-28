7d785df5a8a153e34ca5d9b8ebffa014594a7331
dolphimolphi <mariia.marenko@gmail.com>
2022-07-28 18:32:23 +0300

    add locators to registration form

--- pages/locators.py
+++ pages/locators.py
@@ -1,4 +1,10 @@
 from selenium.webdriver.common.by import By
 
+
 class MainPageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR, '#login_link') #создание пары селекторов
+    LOGIN_LINK = (By.CSS_SELECTOR, '#login_link')  # создание пары селекторов
+    
+
+class LoginPageLocators():
+    LOGIN_FORM_LINK = (By.CSS_SELECTOR, '#login_form')
+    REGISTRATION_FORM_LINK = (By.CSS_SELECTOR, '#register_form')
