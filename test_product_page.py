afa35f3256b5571da9aab45a5c80d4ebe80e4876
dolphimolphi <mariia.marenko@gmail.com>
2022-08-01 20:57:51 +0300

    add parametrize for promo link

--- test_product_page.py
+++ test_product_page.py
@@ -1,9 +1,20 @@
 import pytest
+
 from .pages.product_page import ProductPage
 
 
-def test_guest_can_add_product_to_basket(browser):
-    link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/?promo=newYear2019'
+@pytest.mark.parametrize('link', ["?promo=offer0",
+                                  "?promo=offer1",
+                                  "?promo=offer2",
+                                  "?promo=offer3",
+                                  "?promo=offer4",
+                                  "?promo=offer5",
+                                  "?promo=offer6",
+                                  pytest.param("?promo=offer7", marks=pytest.mark.xfail),
+                                  "?promo=offer8",
+                                  "?promo=offer9"])
+def test_guest_can_add_product_to_basket(browser, link):
+    link = f'http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/{link}'
     page = ProductPage(browser, link)
     page.open()
     page.add_item_to_basket()
