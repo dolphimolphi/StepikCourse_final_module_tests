2acd686c71bab6f1426b5dea491fbb14274955fc
dolphimolphi <mariia.marenko@gmail.com>
2022-08-03 19:04:27 +0300

    add test_guest_cant_see_product_in_basket_opened_from_main_page

--- test_main_page.py
+++ test_main_page.py
@@ -1,12 +1,12 @@
-from .pages.main_page import MainPage
+from pages.base_page import BasePage
 from .pages.login_page import LoginPage
-
+from .pages.basket_page import BasketPage
 
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link) # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open() # открываем страницу
+    page = BasePage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()  # открываем страницу
     page.go_to_login_page()
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
@@ -14,8 +14,15 @@ def test_guest_can_go_to_login_page(browser):
 
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
+    page = BasePage(browser, link)
     page.open()
     page.should_be_login_link()
 
 
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    link = 'http://selenium1py.pythonanywhere.com/'
+    page = BasketPage(browser, link)
+    page.open()
+    page.open_basket_page()
+    page.no_products_in_the_basket()
+    page.should_be_text_is_basket_empty()
