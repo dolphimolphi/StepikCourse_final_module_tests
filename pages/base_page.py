a0069fd959d32898a190a5e986db96875211ba84
dolphimolphi <mariia.marenko@gmail.com>
2022-08-08 20:33:42 +0300

    add def should_be_authorized_user

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,9 +1,9 @@
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.common.exceptions import NoAlertPresentException
-import math
 from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import math
 from .locators import BasePageLocators
 
 
@@ -30,19 +30,19 @@ class BasePage():
             return False
         return True
 
-    # def solve_quiz_and_get_code(self):
-    #     alert = self.browser.switch_to.alert
-    #     x = alert.text.split(" ")[2]
-    #     answer = str(math.log(abs((12 * math.sin(float(x))))))
-    #     alert.send_keys(answer)
-    #     alert.accept()
-    #     try:
-    #         alert = self.browser.switch_to.alert
-    #         alert_text = alert.text
-    #         print(f"Your code: {alert_text}")
-    #         alert.accept()
-    #     except NoAlertPresentException:
-    #         print("No second alert presented")
+    def solve_quiz_and_get_code(self):
+        alert = self.browser.switch_to.alert
+        x = alert.text.split(" ")[2]
+        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        alert.send_keys(answer)
+        alert.accept()
+        try:
+            alert = self.browser.switch_to.alert
+            alert_text = alert.text
+            print(f"Your code: {alert_text}")
+            alert.accept()
+        except NoAlertPresentException:
+            print("No second alert presented")
 
     def is_not_element_present(self, how, what, timeout=4):
         try:
@@ -64,3 +64,7 @@ class BasePage():
     def open_basket_page(self):
         basket_link = self.browser.find_element(*BasePageLocators.BASKET_LINK)
         basket_link.click()
+
+    def should_be_authorized_user(self):
+        assert self.is_element_present(*BasePageLocators.USER_ICON), "User icon is not presented," \
+                                                                     " probably unauthorised user"
