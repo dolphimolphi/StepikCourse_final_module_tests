f6baeb69dd0c7ec42e058b0724f568fe0f4a086d
dolphimolphi <mariia.marenko@gmail.com>
2022-08-08 20:31:01 +0300

    add def register_new_user and asserts

--- pages/login_page.py
+++ pages/login_page.py
@@ -10,14 +10,15 @@ class LoginPage(BasePage):
 
     def should_be_login_url(self):
         assert 'login' in self.browser.current_url
-        # реализуйте проверку на корректный url адрес
-        #assert True
 
     def should_be_login_form(self):
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM_LINK), "Login form is not presented"
 
-
     def should_be_register_form(self):
         assert self.is_element_present(*LoginPageLocators.REGISTRATION_FORM_LINK), "Register form is not presented"
-        # реализуйте проверку, что есть форма регистрации на странице
-        #assert True
+
+    def register_new_user(self, email, password):
+        self.browser.find_element(*LoginPageLocators.EMAIL_LINK).send_keys(email)
+        self.browser.find_element(*LoginPageLocators.PASSWORD_LINK).send_keys(password)
+        self.browser.find_element(*LoginPageLocators.PASSWORD_LINK_CHECKS).send_keys(password)
+        self.browser.find_element(*LoginPageLocators.REGISTER_BUTTON).click()
